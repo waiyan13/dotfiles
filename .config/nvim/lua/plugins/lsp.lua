4bf4f03828e4be9f94eca6682199320ac3381f00
Zaw W. Lwin <zaw.waiyan.lwin96@gmail.com>
2024-06-28 04:45:59 +0700

    feat[LSP]: Add Lua language server

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -61,6 +61,33 @@ return {
                 },
             })
             --]]
+
+            --[[
+            lsp.lua_ls.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                on_init = function(client)
+                    local path = client.workspace_folders[1].name
+                    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                        return
+                    end
+                    client.config.settings.lua = vim.tbl_deep_extend('force', client.config.settings.lua, {
+                        runtime = {
+                            version = "LuaJIT"
+                        },
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                vim.env.VIMRUNTIME
+                            },
+                        },
+                    })
+                end,
+                settings = {
+                    Lua = {}
+                }
+            })
+            --]]
         end,
     },
     {
